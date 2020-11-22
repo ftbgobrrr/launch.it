0aa19cde5e5bfa22650af647687b28d9000cff43
root <admin@hennequince.fr>
2020-11-22 15:05:02 +0100

    Remove test file

--- backend/test.js
+++
@@ -1,6 +0,0 @@
-const bcrypt = require('bcrypt');
-
-
-const r = bcrypt.compareSync('matine74', '$2y$10$KSHXZFmCFxGTSWTge2urPuyRGyMAJjaXJ.6p1QiDzMO8oGmDMH4Qm');
-var hash = bcrypt.hashSync("lol", 10);
-console.log(r, hash);
